b9ae4579c0b0f55e5b404d22747b44fa55d9fae1
Riou Sebastien <sebastien.riou@univ-grenoble-alpes.fr>
2021-09-30 11:03:32 +0200

    Update augcomUpdate.sh

--- Scripts/augcomUpdate.sh
+++ Scripts/augcomUpdate.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-
 NEW_VERSION_LINK=$(curl -s https://api.github.com/repos/AFSR/AugCom-AFSR/releases/latest | grep "browser_download_url.*AugCom*" | cut -d: -f2,3 | tr -d \")
 
 NEW_VERSION=$( echo ${NEW_VERSION_LINK} | cut -d/ -f9)
 
-NEW_VERSION_NO_EXT=$( echo ${NEW_VERSION} | cut -d. -f1,2,3)
+NEW_VERSION_NO_EXT=$( echo ${NEW_VERSION} | cut -d. -f1)
 
-cd ../
+cd ../dist
 
 echo "\n téléchargement de la version ${NEW_VERSION_NO_EXT} en utilisant le lien ${NEW_VERSION_LINK} \n"
 
@@ -18,8 +16,4 @@ tar -zxvf ${NEW_VERSION}
 
 echo "\n supression de l'ancienne version \n"
 
-ls | grep "AugCo.*" | egrep -v "^(${NEW_VERSION_NO_EXT}$)" | xargs rm -r
-
-cd ../interaactionBox_Interface-linux/bin
-
-echo "../../${NEW_VERSION_NO_EXT}" > configuration.conf
+ls | grep "AugCom.*" | egrep -v "^(${NEW_VERSION_NO_EXT}$)" | xargs rm -r
