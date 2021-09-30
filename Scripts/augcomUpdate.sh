e177e83aed037742a59b63f34edddb00bbd2693b
kuhlkrein <sebastien.riou@univ-grenoble-alpes.fr>
2021-09-30 10:24:27 +0200

    augcom and interaactionscene script update

--- Scripts/augcomUpdate.sh
+++ Scripts/augcomUpdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NEW_VERSION_LINK=$(curl -s https://api.github.com/repos/AFSR/GazePlay-AFSR/releases/latest | grep "browser_download_url.*gazeplay-linux*" | cut -d: -f2,3 | tr -d \")
+NEW_VERSION_LINK=$(curl -s https://api.github.com/repos/AFSR/AugCom-AFSR/releases/latest | grep "browser_download_url.*AugCom*" | cut -d: -f2,3 | tr -d \")
 
 NEW_VERSION=$( echo ${NEW_VERSION_LINK} | cut -d/ -f9)
 
@@ -18,7 +18,7 @@ tar -zxvf ${NEW_VERSION}
 
 echo "\n supression de l'ancienne version \n"
 
-ls | grep "gazeplay-linux.*" | egrep -v "^(${NEW_VERSION_NO_EXT}$)" | xargs rm -r
+ls | grep "AugCo.*" | egrep -v "^(${NEW_VERSION_NO_EXT}$)" | xargs rm -r
 
 cd ../interaactionBox_Interface-linux/bin
 
--- Scripts/interAACtionSceneUpdate.sh
+++ Scripts/interAACtionSceneUpdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NEW_VERSION_LINK=$(curl -s https://api.github.com/repos/AFSR/GazePlay-AFSR/releases/latest | grep "browser_download_url.*gazeplay-linux*" | cut -d: -f2,3 | tr -d \")
+NEW_VERSION_LINK=$(curl -s https://api.github.com/repos/AFSR/InteraactionScene-AFSR/releases/latest | grep "browser_download_url.*InteraactionScene*" | cut -d: -f2,3 | tr -d \")
 
 NEW_VERSION=$( echo ${NEW_VERSION_LINK} | cut -d/ -f9)
 
@@ -18,7 +18,7 @@ tar -zxvf ${NEW_VERSION}
 
 echo "\n supression de l'ancienne version \n"
 
-ls | grep "gazeplay-linux.*" | egrep -v "^(${NEW_VERSION_NO_EXT}$)" | xargs rm -r
+ls | grep "InteraactionScen.*" | egrep -v "^(${NEW_VERSION_NO_EXT}$)" | xargs rm -r
 
 cd ../interaactionBox_Interface-linux/bin
 
