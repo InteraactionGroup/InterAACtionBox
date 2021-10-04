b7b09d194c484164e90d706c101dbda61c9511b9
Riou Sebastien <sebastien.riou@univ-grenoble-alpes.fr>
2021-10-04 10:29:37 +0200

    Update gazeplayUpdate.sh

--- Scripts/gazeplayUpdate.sh
+++ Scripts/gazeplayUpdate.sh
@@ -6,7 +6,7 @@ NEW_VERSION=$( echo ${NEW_VERSION_LINK} | cut -d/ -f9)
 
 NEW_VERSION_NO_EXT=$( echo ${NEW_VERSION} | cut -d. -f1,2,3)
 
-cd ../
+cd ~/
 
 echo "\n téléchargement de la version ${NEW_VERSION_NO_EXT} en utilisant le lien ${NEW_VERSION_LINK} \n"
 
@@ -20,6 +20,6 @@ echo "\n supression de l'ancienne version \n"
 
 ls | grep "gazeplay-linux.*" | egrep -v "^(${NEW_VERSION_NO_EXT}$)" | xargs rm -r
 
-cd ../interaactionBox_Interface-linux/bin
+cd ~/interaactionBox_Interface-linux/bin
 
 echo "../../${NEW_VERSION_NO_EXT}" > configuration.conf
