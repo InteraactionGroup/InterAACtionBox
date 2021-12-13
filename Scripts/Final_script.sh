2036b55cd1ccc4117fdc5cdcc002caf6ecfb1478
Riou Sebastien <rsebastien07@gmail.com>
2021-12-13 16:16:11 +0100

    Update Final_script.sh

--- Scripts/Final_script.sh
+++ Scripts/Final_script.sh
@@ -14,29 +14,24 @@ echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
 cd Libs/
 
-# apt install ./dos2unix_7.4.0-2_amd64.deb ./gconf2_3.2.6-6ubuntu1_amd64.deb ./gconf2-common_3.2.6-6ubuntu1_all.deb ./gconf-service_3.2.6-6ubuntu1_amd64.deb ./gconf-service-backend_3.2.6-6ubuntu1_amd64.deb ./libappindicator1_12.10.1+18.04.20180322.1-0ubuntu1_amd64.deb ./libdbusmenu-gtk4_16.04.1+18.04.20171206-0ubuntu1_amd64.deb ./libgconf-2-4_3.2.6-6ubuntu1_amd64.deb ./libindicator7_12.10.2+16.04.20151208-0ubuntu1_amd64.deb ./TobiiProEyeTrackerManager-2.1.2.deb ./multiarch-support_2.27-3ubuntu1_amd64.deb ./google-chrome-stable_current_amd64.deb ./xdotool_3.20160805.1-4_amd64.deb
+#apt -y install ./dos2unix_7.4.0-2_amd64.deb ./gconf2_3.2.6-6ubuntu1_amd64.deb ./gconf2-common_3.2.6-6ubuntu1_all.deb ./gconf-service_3.2.6-6ubuntu1_amd64.deb ./gconf-service-backend_3.2.6-6ubuntu1_amd64.deb ./libappindicator1_12.10.1+18.04.20180322.1-0ubuntu1_amd64.deb ./libdbusmenu-gtk4_16.04.1+18.04.20171206-0ubuntu1_amd64.deb ./libgconf-2-4_3.2.6-6ubuntu1_amd64.deb ./libindicator7_12.10.2+16.04.20151208-0ubuntu1_amd64.deb ./TobiiProEyeTrackerManager-2.1.2.deb ./multiarch-support_2.27-3ubuntu1_amd64.deb ./google-chrome-stable_current_amd64.deb ./xdotool_3.20160805.1-4_amd64.deb
 
 echo "deb http://fr.archive.ubuntu.com/ubuntu/ focal main restricted universe multiverse
 deb http://security.ubuntu.com/ubuntu/ focal-security main restricted
 deb http://archive.ubuntu.com/ubuntu/ focal-updates main restricted
 " > /etc/apt/sources.list
 apt-get update
-apt-get install build-essential
-apt-get install ./*.deb
-apt-get install google-chrome-stable
+apt-get -y install build-essential
+apt-get -y install curl
+apt-get -y install ./*.deb
+apt-get -y install google-chrome-stable
 
 
 # /********************************************************************************************************/
 # /* Part2 : Set-Up GazePlay, InteraactionScene and Tobii */
 
 cp -r ~/gazeplay-linux-x64-1.8.1-AFSR /etc/skel/
-cd /etc/skel/gazeplay-linux-x64-1.8.1-AFSR/tobiiDrivers/drivers/deps/
-apt-get install ./libsqlcipher0_3.4.1-1build1_amd64.deb ./libuv0.10_0.10.22-2_amd64.deb
-
-cd ../
-apt install ./tobii_engine_linux-0.1.6.193_rc-Linux.deb ./tobiiusbservice_l64U14_2.1.5-28fd4a.deb
-
-cd ../../lib/jre/bin/
+cd /etc/skel/gazeplay-linux-x64-1.8.1-AFSR/lib/jre/bin/
 chmod +x java
 
 cd ../../../bin
@@ -73,14 +68,16 @@ cd .config
 mkdir autostart
 
 cd /etc/skel/
-mkdir .local
+mkdirs .local/
+mkdirs .local/share
+mkdirs .local/share/applications
 cp ~/Ressources/DesktopFiles/*.desktop  .local/share/applications
 chmod a+x /etc/skel/.local/share/applications
-cp ~/Ressources/DesktopFiles/interaactionBoxLauncher.desktop /etc/skel/.config/autostart
-chmod a+x /etc/skel/.config/autostart/interaactionBoxLauncher.desktop
+cp ~/Ressources/DesktopFiles/InteraactionBoxLauncher.desktop /etc/skel/.config/autostart
+chmod a+x /etc/skel/.config/autostart/InteraactionBoxLauncher.desktop
 
 cp -R ~/Ressources/Launcher /etc/skel/
-chmod a+x /etc/skel/launcher/*
+chmod a+x /etc/skel/Launcher/*
 
 # dbus-launch gio set InteraactionBoxLauncher.desktop "metadata::trusted" true
 
@@ -129,11 +126,9 @@ cp /usr/share/localechooser/shortlists ./
 echo "yes" > /etc/skel/.config/gnome-initial-setup-done
 #adduser localadmin --group sudo
 
-#Do it also for dock and for show-trash false and show-home false
-
 # cd /usr/share/polkit-1/actions/
 # gedit org.freedesktop.NetworkManager.policy
-# System policy prevents modification of netwrk settings for all users
+# System policy prevents modification of network settings for all users
 # and change to <allow_active>yes</allow_active>
 
 
